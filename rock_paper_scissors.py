734c9037d6c4d30c653647133c1f9cbffacf5a6b
jcnghm <jcnghmpsEgmail.com>
2021-08-20 15:29:03 -0700

    Score Adjustments

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -20,7 +20,7 @@ while True:
         if computer_move == "paper":
             print(f'You lose! The computer selected {computer_move}, {computer_move} covers rock! ')
             computer_score+=1
-            if computer_score == 3:
+            if computer_score == 2:
                 print(f'Game Over! Final score: Computer: {computer_score} User: {user_score} ')
                 computer_score = 0
                 user_score = 0
@@ -28,7 +28,7 @@ while True:
         else:
             print(f'You win! {user_move} smashes {computer_move}! ')
             user_score+=1
-            if user_score == 3:
+            if user_score == 2:
                 print(f'Congratulations! You win! Final score: User: {user_score} Computer: {computer_score}!')
                 computer_score = 0
                 user_score = 0
@@ -37,7 +37,7 @@ while True:
         if computer_move == "scissors":
             print(f'You lose! The computer selected {computer_move}, {computer_move} cuts {user_move}! ')
             computer_score+=1
-            if computer_score == 3:
+            if computer_score == 2:
                 print(f'Game Over! Final score: Computer: {computer_score} User: {user_score} ')
                 computer_score = 0
                 user_score = 0
@@ -45,7 +45,7 @@ while True:
         else:
             print(f'You win! {user_move} covers {computer_move}! ')
             user_score+=1
-            if user_score == 3:
+            if user_score == 2:
                 print(f'Congratulations! You win! Final score: User: {user_score} Computer: {computer_score}!')
                 computer_score = 0
                 user_score = 0
@@ -54,7 +54,7 @@ while True:
         if computer_move == 'rock':
             print(f'You lose! The computer selected {computer_move}, {computer_move} smashes {user_move}! ')
             computer_score+=1
-            if computer_score == 3:
+            if computer_score == 2:
                 print(f'Game Over! Final score: Computer: {computer_score} User: {user_score} ')
                 computer_score = 0
                 user_score = 0
@@ -62,7 +62,7 @@ while True:
         else:
             print(f'You win! {user_move} cuts {computer_move}! ')
             user_score+=1
-            if user_score == 3:
+            if user_score == 2:
                 print(f'Congratulations! You win! Final score: User: {user_score} Computer: {computer_score}!')
                 computer_score = 0
                 user_score = 0
